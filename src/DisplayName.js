6e17b9df6480a20bcb9d59dfe5e1a1d88c6702f7
pradhyumansinh <jadejapradhyumansinh545@gmail.com>
2024-05-20 23:44:47 +0530

    test case fails correct it

--- src/DisplayName.js
+++ src/DisplayName.js
@@ -23,7 +23,7 @@ const DisplayName = () => {
             <h1>Full Name Display</h1>
             <label for="firstName">First Name</label>: <input type="text" required name="firstName" id="firstName" onChange={handleFillName} value={firstName} /><br></br>
             <label for="lastName">Last Name</label>: <input type="text" required name="lastName" id="lastName" onChange={handleFillName} value={lastName} /><br></br>
-            <button type="submit">Submit</button><br></br>
+            <button type="submit">Submit</button><br></br><br></br>
             <label>Full Name</label>: <label>{fullName}</label>
         </form>
     );
