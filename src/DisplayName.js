a6ac2c9a45fddcfba9a4df6ef3ea1cdf4d916614
pradhyumansinh <jadejapradhyumansinh545@gmail.com>
2024-05-20 23:38:13 +0530

    correct form event

--- src/DisplayName.js
+++ src/DisplayName.js
@@ -19,11 +19,11 @@ const DisplayName = () => {
     }
 
     return (
-        <form>
+        <form onSubmit={handleDisplayName}>
             <h1>Full Name Display</h1>
-            <label for="firstName">First Name</label>: <input type="text" name="firstName" id="firstName" onChange={handleFillName} value={firstName} /><br></br>
-            <label for="lastName">Last Name</label>: <input type="text" name="lastName" id="lastName" onChange={handleFillName} value={lastName} /><br></br>
-            <button onClick={handleDisplayName}>Submit</button><br></br>
+            <label for="firstName">First Name</label>: <input type="text" required name="firstName" id="firstName" onChange={handleFillName} value={firstName} /><br></br>
+            <label for="lastName">Last Name</label>: <input type="text" required name="lastName" id="lastName" onChange={handleFillName} value={lastName} /><br></br>
+            <button type="submit">Submit</button><br></br>
             <label>Full Name</label>: <label>{fullName}</label>
         </form>
     );
