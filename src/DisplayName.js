29fa47ef890b9f88f52cefd44f7e9e0b9edd297f
pradhyumansinh <jadejapradhyumansinh545@gmail.com>
2024-06-08 23:05:46 +0530

    add flags with else condition

--- src/DisplayName.js
+++ src/DisplayName.js
@@ -3,34 +3,30 @@ import { useState } from 'react';
 const DisplayName = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [fullName, setFullName] = useState("");
     const [flag, setFlag] = useState(false);
+    const [fullName, setFullName] = useState("");
 
-    const handleFillName = (e) => {
-        if (e.target.id === "firstName") {
-            setFirstName(e.target.value);
-        } else {
-            setLastName(e.target.value);
-        }
-    }
 
     const handleDisplayName = (e) => {
         e.preventDefault();
         if (firstName && lastName) {
             setFlag(true);
+            setFullName(`Full Name: ${firstName} ${lastName}`);
+        } else {
+            setFlag(false);
         }
-        setFullName(`Full Name: ${firstName}  ${lastName}`);
     }
 
     return (
-        <form onSubmit={handleDisplayName}>
-            <h1>Full Name Display</h1>
-            <label for="firstName">First Name</label>: <input type="text" required name="firstName" id="firstName" onChange={handleFillName} value={firstName} /><br></br>
-            <label for="lastName">Last Name</label>: <input type="text" required name="lastName" id="lastName" onChange={handleFillName} value={lastName} /><br></br>
-            <button type="submit">Submit</button><br></br>
+        <div>
+            <form onSubmit={handleDisplayName}>
+                <h1>Full Name Display</h1>
+                <label for="firstName">First Name</label>: <input type="text" required name="firstName" id="firstName" onChange={(e) => setFirstName(e.target.value)} value={firstName} /><br></br>
+                <label for="lastName">Last Name</label>: <input type="text" required name="lastName" id="lastName" onChange={(e) => setLastName(e.target.value)} value={lastName} /><br></br>
+                <button type="submit">Submit</button><br></br>
+            </form>
             {flag && (<p>{fullName}</p>)}
-
-        </form>
+        </div>
     );
 }
 
